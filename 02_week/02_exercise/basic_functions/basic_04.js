0434a44fbc7db4cf3b1d72abb670bd2aa82dbb8f
Savolainen Andrei <s2400800@BC-C02GR0BXQ6LRs-MacBook-Air.local>
2024-08-21 14:03:52 +0300

    initial

--- 02_week/02_exercise/basic_functions/basic_04.js
+++ 02_week/02_exercise/basic_functions/basic_04.js
@@ -1,9 +1,10 @@
 // Create a function named isEven that takes a number as a parameter and returns true if the number is even, and false if the number is odd.
 
-function isEven(number) {
-    return number % 2 == 0;
-}
+// function isEven(number) {
+//     return number % 2 === 0; //checking value and the type. Returns true or false.
+// }
 
+const isEven = (number) => number % 2 === 0;
 
 // sample usage - do not modify
 console.log(isEven(4)); // Returns true
--- 02_week/02_exercise/basic_functions/basic_05.js
+++ 02_week/02_exercise/basic_functions/basic_05.js
@@ -1,8 +1,11 @@
 // Write a function named findMax that takes three numbers as parameters and returns the largest of the three.
 
-function findMax(a, b, c) {
-    return Math.max(a, b, c);
-}
+// function findMax(a, b, c) {
+//     return Math.max(a, b, c);
+// }
+
+const findMax = (a, b, c) => Math.max(a, b, c);
+
 
 // sample usage - do not modify
 console.log(findMax(3, 7, 2)); // Returns 7

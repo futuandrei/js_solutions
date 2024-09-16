7102a6aba6c55660e592bd3927554ef736073849
Savolainen Andrei <s2400800@BC-C02GR0BXQ6LRs-MacBook-Air.local>
2024-09-16 09:21:16 +0300

    delete 04_week tests and add 05_exercises(except Task 6 not finished)

--- 05_week/05_exercises/objects_2.js
+++ 05_week/05_exercises/objects_2.js
@@ -8,13 +8,18 @@ Add at least two book objects to this collection.
 
 // Your code here
 
-// const library [
-//     const book = {
-//         title: '',
-//         author: '',
-//         yearPublished: ''
-//     };
-// ];
+const library = [
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        yearPublished: 1960
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        yearPublished: 1949
+    }
+];
 
 /* Task 2
 Access the `title` of the first book in the `library` collection and log it to the console.
@@ -22,6 +27,7 @@ Modify the `yearPublished` for the second book in the collection to a new year, 
 */
 
 // Your code here
+console.log(library[0].title);
 
 /* Task 3
 Use dot notation to add a new property `genres` (an array of strings) to the first book in the `library`.
@@ -29,6 +35,10 @@ Use bracket notation to add a boolean property `isAvailable` to the same book, i
 */
 
 // Your code here
+library[0].genres = [];
+library[0]["isAvailable"] = true;
+console.log(library[0]);
+
 
 /* Task 4
 Define a constructor function named `Book` that can create new book objects with properties for `title`, `author`, `yearPublished`, and `genres`.
@@ -37,6 +47,18 @@ Using the `Book` constructor, create a new book object with the provided input v
 
 // Your code here
 
+// function Book(title, author, yearPublished, genres) {
+//     this.title = title;
+//     this.author = author;
+//     this.yearPublished = yearPublished;
+//     this.genres = genres;
+// }
+// let newBook = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, ["Fiction", "Classic", "Tragedy"]);
+
+// library.push(newBook);
+
+// console.log(newBook);
+
 /* Task 5
 Write a function named `createBook` that takes parameters for `title`, `author`, `yearPublished`, and `genres` (an array).
 The function should return a new book object with these properties.
@@ -44,6 +66,23 @@ Test `createBook` by creating a new book object with user-provided input and log
 */
 
 // Your code here
+
+// Define the createBook function
+function createBook(title, author, yearPublished, genres) {
+    return {
+        title: title,
+        author: author,
+        yearPublished: yearPublished,
+        genres: genres
+    };
+}
+
+// Test createBook by creating a new book object with user-provided input
+let newBook = createBook("Moby Dick", "Herman Melville", 1851, ["Fiction", "Adventure", "Classic"]);
+
+// Log the new book object to the console
+console.log(newBook);
+
 
 /* Task 6
 Convert the `library` collection to a JSON string and log it to the console.

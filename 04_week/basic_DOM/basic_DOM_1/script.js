891f50c324b9f23ca0d40715295393845f18b0b1
Savolainen Andrei <s2400800@BC-C02GR0BXQ6LRs-MacBook-Air.local>
2024-09-16 09:24:17 +0300

    adding modified

--- 04_week/basic_DOM/basic_DOM_1/script.js
+++ 04_week/basic_DOM/basic_DOM_1/script.js
@@ -3,7 +3,18 @@ Write JavaScript code that selects content id by using getElementById.
 Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
 */
 
-function updateText() {
-    document.getElementById("content").innerHTML = "Hello World";
+// Function without event listeners
+// function updateText() {
+//     document.getElementById('content').innerHTML = "Hello World!"
+// }
 
+function updateText() {
+    document.getElementById("content").innerHTML = "Hello World!"
 }
+
+// Function with event listener
+// 1. Declare button variable and assign correct element to it
+const element = document.getElementById("updateText");
+// 2. Create event listener
+element.addEventListener("click", updateText);
+

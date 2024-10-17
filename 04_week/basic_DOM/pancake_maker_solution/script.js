3101a5557a38498d0a23d6d873857d6e07645d4d
Savolainen Andrei <s2400800@BC-C02GR0BXQ6LRs-MacBook-Air.local>
2024-10-17 16:00:06 +0300

    add first changes to pancakes

--- 04_week/basic_DOM/pancake_maker_solution/script.js
+++ 04_week/basic_DOM/pancake_maker_solution/script.js
@@ -1,16 +1,34 @@
 
-// Get input and checkboxes
-const pancakeType = document.getElementById("type");
-const toppingSelection = document.querySelector("#nuts");
+// Get input
+const pancakeType = document.getElementById('type');
+// Get toppings checkboxes
+const toppingCheckboxes = document.querySelectorAll('.customization-section:nth-of-type(2) input[type="checkbox"]');
+// Get extras checkboxes
+const extrasCheckboxes = document.querySelectorAll('.customization-section:nth-of-type(3) input[type="checkbox"]');
+// Get total price
+const totalPrice = document.querySelector('.price-display span');
 
-// Event listeners
-pancakeType.addEventListener("change", doSomething);
-toppingSelection.addEventListener("change", doSomething);
 
-// function 
-function doSomething() {
+// console.log("first " + typeof (toppingCheckboxes));
+// console.log("second " + typeof (extrasCheckboxes));
+
+// function to calculate base price (TESTING)
+function calcBaseprice() {
     // console.log("Type was changed");
     const value = this.value;
-    console.log(value);
+    totalPrice = value;
 }
 
+// function to calculate toppings price (TESTING)
+function calcToppingsprice() {
+    console.log("toppings is triggered");
+
+}
+
+// Event listeners
+pancakeType.addEventListener('change', calcBaseprice);
+
+// Event listener for toppings
+toppingCheckboxes.forEach(checkbox => {
+    console.log("function was called");
+});
---
+++ 04_week/basic_DOM/pancake_maker_solution/steps.md
@@ -0,0 +1,6 @@
+# Steps
+
+### Looping through checkboxes
+
+Had problem in figuring out how to loop through checkboxes, as there are several in DOM with <div class="customization-section">
+and <input type="checkbox" ... >
